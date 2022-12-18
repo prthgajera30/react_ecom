e363662d1786d229d95590652b44c8b6786dd465
prthgajera30 <prthgajera@gmail.com>
2022-12-18 08:06:38 +0100

    second commit

--- components/products.js
+++ components/products.js
@@ -1,14 +1,15 @@
-import {ProductStyle} from "../styles/ProductStyle";
+import { ProductStyle } from "../styles/ProductStyle";
 
-export default function Products({product}){
-    const { Title, Price, Image} = product.attributes
-    return(
-        <ProductStyle>
-            <div>
-                <img src={process.env.NEXT_PUBLIC_BACKEND_URL+Image.data[0].attributes.formats.small.url}/>
-            </div>
-            <h2>{Title}</h2>
-            <h3>{Price}</h3>
-        </ProductStyle>
-    )
+export default function Products({ product }) {
+  const { Title, Price, Image } = product.attributes;
+  console.log(Image);
+  return (
+    <ProductStyle>
+      <div>
+        <img src={Image.data[0].attributes.formats.small.url} />
+      </div>
+      <h2>{Title}</h2>
+      <h3>{Price} €</h3>
+    </ProductStyle>
+  );
 }
