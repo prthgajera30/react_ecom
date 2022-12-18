7dd9d070daef21bbae1e161f310eb703d14eed2c
prthgajera30 <prthgajera@gmail.com>
2022-12-19 00:59:07 +0100

    no need

--- components/Cart.js
+++ components/Cart.js
@@ -1,26 +1,49 @@
 import { useStateContext } from "../lib/context";
-import { CartWrapper, CartStyle } from "../styles/CartStyle";
+import {
+  CartWrapper,
+  CartStyle,
+  Card,
+  CardInfo,
+  EmptyStyle,
+} from "../styles/CartStyle";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+import { Quantity } from "../styles/ProductDetails";
 
 export default function Cart() {
-  const { cartItems } = useStateContext();
+  const { cartItems, setShowCart, onAdd, onRemove } = useStateContext();
   return (
-    <CartWrapper>
-      <CartStyle>
+    <CartWrapper onClick={() => setShowCart(false)}>
+      <CartStyle onClick={(e) => e.stopPropagation()}>
         {cartItems.length < 1 && (
-          <div>
-            <h1> Shopping Cart</h1>
-          </div>
+          <EmptyStyle>
+            <h1> You have more shopping to do </h1>
+            <FaShoppingCart />
+          </EmptyStyle>
         )}
         {cartItems.length >= 1 &&
           cartItems.map((Item) => {
             return (
-              <div>
-                <img src="" alt="" />
-                <div>
+              <Card key={Item.Slug}>
+                <img
+                  src={Item.Image.data[0].attributes.formats.thumbnail.url}
+                  alt=""
+                />
+                <CardInfo>
                   <h3>{Item.Title}</h3>
-                  <h3>{Item.Price}</h3>
-                </div>
-              </div>
+                  <h3>{Item.Price} €</h3>
+                  <Quantity>
+                    <span>Quantity</span>
+                    <button onClick={() => onRemove(Item)}>
+                      <AiFillMinusCircle />
+                    </button>
+                    <p>{Item.quantity}</p>
+                    <button onClick={() => onAdd(Item, 1)}>
+                      <AiFillPlusCircle />
+                    </button>
+                  </Quantity>
+                </CardInfo>
+              </Card>
             );
           })}
       </CartStyle>
--- components/Nav.js
+++ components/Nav.js
@@ -2,18 +2,20 @@ import Link from "next/link";
 import { FiShoppingBag } from "react-icons/fi";
 import { NavStyle, NavItems } from "../styles/NavStyle";
 import Cart from "./Cart";
+import { useStateContext } from "../lib/context";
 
 export default function Nav() {
+  const { showCart, setShowCart } = useStateContext();
   return (
     <NavStyle>
       <Link href={"/"}>Fashion</Link>
       <NavItems>
-        <div>
+        <div onClick={() => setShowCart(true)}>
           <FiShoppingBag />
           <h3>Cart</h3>
         </div>
       </NavItems>
-      <Cart />
+      {showCart && <Cart />}
     </NavStyle>
   );
 }
