6c8c814804ff4d9d9711e7ce6d484ddd496741ef
Parth <prth@gmail.com>
2022-12-21 02:38:10 +0100

    Initial commit from Create Next App

--- styles/CartStyle.js
+++ styles/CartStyle.js
@@ -18,8 +18,8 @@ export const CartWrapper = styled(motion.div)`
 export const CartStyle = styled(motion.div)`
   width: 30%;
   background-color: #f1f1f1;
-  padding: 2rem 5rem;
-  overflow-y: scroll;
+  padding: 1rem 2.5rem;
+  overflow-y: auto;
   position: relative;
 `;
 
--- styles/ProductStyle.js
+++ styles/ProductStyle.js
@@ -5,13 +5,23 @@ export const ProductStyle = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
-  padding: 1.5rem;
-  
-  img{
+  //padding: 1.5rem;
+  margin: 1.2rem;
+  transition: transform 0.3s ease;
+  border: 0.5px solid #ccc;
+  border-radius: 10px;
+  overflow: hidden;
+
+  :hover {
+    transform: scale(1.2);
+    box-shadow: 0 0 11px rgba(33, 33, 33, 0.2);
+  }
+  img {
     width: 100%;
     cursor: pointer;
   }
-  h2{
-    padding: 0.5rem 0rem;
+  h2,
+  h3 {
+    padding: 0.5rem 1rem;
   }
 `;
